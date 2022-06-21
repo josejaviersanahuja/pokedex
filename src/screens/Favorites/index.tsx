d302d171f38362817cba876d03427584b0a1827e
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-06-21 20:55:47 +0100

    ultimo arreglo

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -65,7 +65,7 @@ export default Favorites;
 const styles = StyleSheet.create({
   main: {
     marginBottom: 40,
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
   },
   texto: {
     padding: 20,
